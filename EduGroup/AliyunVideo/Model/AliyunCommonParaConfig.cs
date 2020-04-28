620792b495a64c8f5f5424a9e518bd8bf67e2b58
jorzen <jorzen2010@163.com>
2020-04-28 11:28:41 +0800

    delidandkey

--- EduGroup/AliyunVideo/Model/AliyunCommonParaConfig.cs
+++ EduGroup/AliyunVideo/Model/AliyunCommonParaConfig.cs
@@ -11,8 +11,8 @@ namespace AliyunVideo
         public const string ApiUrl = "vod.cn-shanghai.aliyuncs.com";
         public const string Format = "JSON";
         public const string Version = "2017-03-21";
-        public const string AccessKeyId = "LTAIHmoHhNfb1Euj";
-        public const string AccessKeySecret = "afCSAjYmNuVcPLTLuwxCtwvu4VvYrr";
+        public const string AccessKeyId = "";
+        public const string AccessKeySecret = "";
 
         public const string SignatureVersion = "1.0";
 
